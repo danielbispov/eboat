770b5158cddb276d938196e4ca69654197b8e9b7
danielbispov <danielbispov@gmail.com>
2018-06-11 00:25:59 -0300

    Configured paths for Eboat

---
+++ application/views/commons/header.php
@@ -0,0 +1,24 @@
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Eboat application common header">
+        <meta name="author" content="Daniel Vale, Renan Barroncas, Rodrigo Selistre">
+        <title>Eboat</title>
+        <link rel="stylesheet" type="text/css" href="<?=base_url("assets/css/bootstrap.min.css")?>">
+        <script src="<?=base_url("assets/js/bootstrap.js")?>"></script>
+    </head>
+
+    <body>
+        <header class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="navbar-brand w-100 order-1 order-md-0 dual-collapse2">
+                <h1 class="navbar-brand">
+                    <a class="nav-link" href="#">ENEM Quiz 2018</a>
+                </h1>
+            </div>
+            <ul class="justify-content-end">
+                <li id="author_item" class="nav-item">
+                    <a id="author" class="nav-link" href="#">Autor</a>
+                </li>
+            </ul>
+        </header>
---
+++ application/views/home.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php $this->load->view('commons/header'); ?>
+</html>
