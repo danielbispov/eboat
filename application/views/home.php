4d86bff3142c4cd8f18ba857dc335bc87f63678f
danielbispov <danielbispov@gmail.com>
2018-06-19 19:17:05 -0300

    Add login view

--- application/views/home.php
+++ application/views/home.php
@@ -9,8 +9,8 @@
         <div class="jumbotron bg-transparent">
             <h1 class="display-4">Follow your destiny</h1>
             <p>What are you up to?</p>
-            <button class="btn btn-outline-dark btn-lg">Share boat</button>
-            <button id="ride-btn" class="btn btn-dark btn-lg">Get a ride</button>
+            <button class="btn btn-outline-dark">Share boat</button>
+            <button id="ride-btn" class="btn btn-dark">Get a ride</button>
         </div>
     </div>
 
