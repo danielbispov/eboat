b3811320619c61ac9dd0592cf97945313c6eac11
danielbispov <danielbispov@gmail.com>
2018-06-20 00:43:02 -0300

    Add few modifications do view, trying to make login feature, need to config variables

---
+++ application/models/Eboat_model.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 19/06/18
+ * Time: 22:38
+ */
+
+class Eboat_model
+{
+
+}
---
+++ application/views/logged_home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php $this->load->view('commons/header');?>
+
+    <body>
+    <?php $this->load->view('commons/menu'); ?>
+    <div class="container">
+        <?php  ?>
+        <div class="jumbotron bg-transparent">
+            <h1 class="display-4">Follow your destiny</h1>
+            <p>What are you up to?</p>
+            <button class="btn btn-outline-dark">Share boat</button>
+            <button id="ride-btn" class="btn btn-dark">Get a ride</button>
+        </div>
+    </div>
+
+    </body>
+</html>
