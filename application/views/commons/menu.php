6f7f015ccfed95aaf7a9c838a0d67da0cae645fa
danielbispov <danielbispov@gmail.com>
2018-06-22 17:19:27 -0300

    Add/remove stuff for w3c validation and add required to forms

--- application/views/commons/menu.php
+++ application/views/commons/menu.php
@@ -17,6 +17,9 @@
                             "<a class=\"nav-link\" href=\"" . base_url('login') . "\">Sign In</a>" .
                             "</li>";
                     }?>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?=base_url('signup'); ?>">Sign Up</a>
+                </li>
             </ul>
         </div>
     </div>
