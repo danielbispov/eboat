4d7a63556f82d639e107ffae83d7db70469a68fb
danielbispov <danielbispov@gmail.com>
2018-06-22 17:48:29 -0300

    Add/remove stuff for w3c validation

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -297,7 +297,7 @@
         <?php endif; ?>
 
         <!-- Cancel trip modal -->
-        <div class="modal fade" id="modal_cancel" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal fade" id="modal_cancel" tabindex="-1" role="dialog" aria-labelledby="123" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
